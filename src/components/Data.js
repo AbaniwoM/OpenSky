0fcb3a253fabeb462002634db908723e846721a3
AbaniwoM <michaelabaniwo@gmail.com>
2022-04-11 14:02:41 +0100

    updated API

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,53 +1,100 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-// var BaseUrl = "https://opensky-network.org/api/";
+const baseUrl = "https://opensky-network.org/api/";
+const getAll = "states/all";
+const arrivalInASpecificAirport =
+  "flights/arrival?airport=EDDF&begin=1517227200&end=1517230800";
+const departureFromASpecificAirport =
+  "flights/departure?airport=EDDF&begin=1517227200&end=1517230800";
 
 const Data = () => {
-  const [flights, setFlights] = useState([]);
+  const [flightsTime, setFlightsTime] = useState([]);
+  const [flightsArrival, setFlightsArrival] = useState([]);
+  const [flightsDeparture, setFlightsDeparture] = useState([]);
 
-  const getAll = "https://opensky-network.org/api/"
-  const airportArrivals = 'https://opensky-network.org/api/'
+  // flightsTime.states.map((flight) => {
+  //   // console.log(flight.states);
+  // });
 
-  console.log(flights)
-  console.table(flights.states)
-
-  useEffect(() => {
-    flightDetails();
-  }, []);
-
-  async function flightDetails() {
+  async function flightTime() {
     try {
-        const res = await fetch("https://opensky-network.org/api/states/all");
-        const data = await res.json();
-        setFlights(data);
+      const res = await fetch(`${baseUrl + getAll}`);
+      const data = await res.json();
+      setFlightsTime(data);
     } catch (err) {
-        console.error(err);
+      console.error(err);
     }
   }
-  
+  async function flightArrivalDetails() {
+    try {
+      const res = await fetch(`${baseUrl + arrivalInASpecificAirport}`);
+      const data = await res.json();
+      setFlightsArrival(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  async function flightDepartureDetails() {
+    try {
+      const res = await fetch(`${baseUrl + departureFromASpecificAirport}`);
+      const data = await res.json();
+      setFlightsDeparture(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    flightTime();
+    flightArrivalDetails();
+    flightDepartureDetails();
+  }, []);
+
+  // all flights at a given time
+  var unixTimestamp = flightsTime.time;
+  var dateObj = new Date(unixTimestamp * 1000);
+  var utcString = dateObj.toUTCString();
+  var time = utcString.slice(-11, -4);
+  console.log(time);
+
+  const startTime = unixTimestamp;
+  const endTime = startTime + 3600;
+  console.log(startTime);
+  console.log(endTime);
+
+  // count the number of flights arriving to a particular airport
+  var arrivalCount = flightsArrival.length;
+  var departureCount = flightsDeparture.length;
+
+  // consoles
+  console.log(flightsArrival);
+  console.log(flightsDeparture);
+  console.log(arrivalCount);
+  console.log(departureCount);
+
   return (
-    <div className="Data">
-        {/* {flights.map((flight, index) => ( */}
-            {/* <div key={index}> */}
-              {/* Airport */}
-              {/* <h2></h2> */}
+    <div
+      className="Data"
+      style={{ paddingTop: 120, display: "flex", color: "white", marginLeft: 25 }}
+    >
+      {/* {flights.map((flight, index) => ( */}
+      {/* <div key={index}> */}
+      {/* Airport */}
+      <h2 style={{ marginRight: 8 }}>DFL</h2>
 
-              {/* Current Time */}
-              {/* <h2></h2> */}
+      {/* Current Time */}
+      <h2 style={{ marginRight: 8 }}>| {time} CAT</h2>
 
-              {/* Number of Flights Arriving */}
-              {/* <h2></h2> */}
+      {/* Number of Flights Arriving */}
+      <h2 style={{ marginRight: 8 }}>| {arrivalCount}</h2>
 
-              {/* Number of Flights Departing */}
-              {/* <h2></h2> */}
-               {/* <h2>{flight.states}</h2> */}
-            {/* </div> */}
-        {/* ))}   */}
+      {/* Number of Flights Departing */}
+      <h2>| {departureCount}</h2>
+      {/* <h2>{flight.states}</h2> */}
+      {/* </div> */}
+      {/* ))}   */}
     </div>
-  )
-}
+  );
+};
 
-export default Data
-
-        
-        
+export default Data;
